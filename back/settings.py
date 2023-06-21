51ffa3ac63e0990de2b4f735c3070fc8223a6d4e
Luis <deathghoul41@gmail.com>
2023-06-21 20:59:32 +0200

    final

--- back/settings.py
+++ back/settings.py
@@ -113,8 +113,12 @@ WSGI_APPLICATION = 'back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD':str(os.getenv('PGPASSWORD')),
+        'HOST': 'containers-us-west-18.railway.app',
+        'PORT': '6768',
     }
 }
 
