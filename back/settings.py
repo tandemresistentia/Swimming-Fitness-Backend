e9a87ecf196d25ad193ca785a07748ce9d7d8b07
Luis <deathghoul41@gmail.com>
2023-06-21 14:55:10 +0200

    final

--- back/settings.py
+++ back/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'https://swimming-fitness-backend-production.up.railway.app/',
     'http://localhost:3000',
-    'https://127.0.0.1',
+    '127.0.0.1',
 ]
 
 CORS_ALLOWED_ORIGINS = [
