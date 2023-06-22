287906b2ab2a425027d02e549b7cc9d4f9641048
Luis <deathghoul41@gmail.com>
2023-06-22 12:47:56 +0200

    final

--- back/settings.py
+++ back/settings.py
@@ -190,9 +190,10 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-from .constants import EMAIL_HOST_PASSWORD
-
+try:
+    from .constants import EMAIL_HOST_PASSWORD
+except:
+    pass
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtppro.zoho.eu'
 EMAIL_USE_SSL = True
